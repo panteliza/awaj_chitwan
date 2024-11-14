b7047bc853da63161f3745ae8ef3200a9ff844ca
Eliza Pant <elizapant.github@gmail.com>
2024-11-14 12:54:29 +0545

    nbash

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,6 +7,12 @@ import 'swiper/css';
 import hearing1 from '../assets/last1.jpg';
 import hearing2 from '../assets/manpic.jpg';
 import hearing3 from '../assets/last3.jpg';
+import hearing4 from '../assets/digi 1.jpeg';
+import hearing5 from '../assets/tinnitus2.jpg';
+import hearing6 from '../assets/oae.png';
+import hearing7 from '../assets/digital hearing aids.jpg';
+import hearing8 from '../assets/Ear-anatomy-with-probe-inserted.jpg';
+ 
 
 export default function Slider() {
   return (
@@ -33,6 +39,31 @@ export default function Slider() {
         <SwiperSlide>
           <div className="w-full">
             <img src={hearing3} className=" h-[500px] sm:h-[640px] 2xl:h-[900px] w-full object-cover" alt="Slider" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full">
+            <img src={hearing4} className=" h-[500px] sm:h-[640px] 2xl:h-[900px] w-full object-cover" alt="Slider" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full">
+            <img src={hearing5} className=" h-[500px] sm:h-[640px] 2xl:h-[900px] w-full object-cover" alt="Slider" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full">
+            <img src={hearing6} className=" h-[500px] sm:h-[640px] 2xl:h-[900px] w-full object-cover" alt="Slider" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full">
+            <img src={hearing7} className=" h-[500px] sm:h-[640px] 2xl:h-[900px] w-full object-cover" alt="Slider" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full">
+            <img src={hearing8} className=" h-[500px] sm:h-[640px] 2xl:h-[900px] w-full object-cover" alt="Slider" />
           </div>
         </SwiperSlide>
       </Swiper>
