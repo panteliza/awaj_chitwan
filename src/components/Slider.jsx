425d90e64e19b64dde37d70f87d186a9ce2cf28c
Eliza Pant <elizapant.github@gmail.com>
2024-11-23 21:44:48 +0545

    arrow removed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import { Autoplay, Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
+
+// Import all images
 import hearing1 from '../assets/last1.jpg';
 import hearing2 from '../assets/manpic.jpg';
 import hearing3 from '../assets/last3.jpg';
 import hearing4 from '../assets/more.png';
 import hearing5 from '../assets/more2.jpeg';
 import hearing6 from '../assets/tyoes.png';
-import hearing7 from '../assets/digital hearing aids.jpg';
-import hearing8 from '../assets/Ear-anatomy-with-probe-inserted.jpg';
+
+// Define the images in an array
+const images = [
+  { src: hearing1, alt: 'Slide 1' },
+  { src: hearing6, alt: 'Slide 2' },
+  { src: hearing2, alt: 'Slide 3' },
+  { src: hearing3, alt: 'Slide 4' },
+  { src: hearing5, alt: 'Slide 5' },
+  { src: hearing4, alt: 'Slide 6' },
+];
 
 export default function Slider() {
   return (
@@ -20,73 +29,20 @@ export default function Slider() {
           delay: 2500,
           disableOnInteraction: false,
         }}
-        navigation={true}
-        modules={[Navigation, Autoplay]}
-        className="object-scale-down swiper-container"
+        modules={[Autoplay]} // Only include the Autoplay module
+        className="swiper-container"
       >
-        <SwiperSlide>
-          <div className="w-full">
-            <img
-              src={hearing1}
-              className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
-              alt="Slider"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full">
-            <img
-              src={hearing6}
-              className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
-              alt="Slider"
-            />
-          </div>
-        </SwiperSlide>
-       
-        <SwiperSlide>
-          <div className="w-full">
-            <img
-              src={hearing2}
-              className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
-              alt="Slider"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full">
-            <img
-              src={hearing3}
-              className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
-              alt="Slider"
-            />
-          </div>
-        </SwiperSlide>
-       
-        
-        <SwiperSlide>
-          <div className="w-full">
-            <img
-              src={hearing5}
-              className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
-              alt="Slider"
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full">
-            <img
-              src={hearing4}
-              className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
-              alt="Slider"
-            />
-          </div>
-        </SwiperSlide>
-        
-      
-       
-       
-       
-       
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="w-full">
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="w-full h-[300px] sm:h-[600px] md:h-[640px] 2xl:h-[900px] object-cover object-center"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
